ef13eb083f140463b0039c66f240bb8fb2d304fe
Yaoghost <118417570+Yaoghost@users.noreply.github.com>
2025-09-05 01:56:36 -0500

    Security

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import { Button } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box } from "@chakra-ui/react";
+import CreatePage from "./pages/CreatePage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <Button>Hello</Button>;
+  return (
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
